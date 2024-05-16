44d03a244292c2b9ac7961d2b7874829ee6caa28
Hemant Chaudhary <hemuchoudhary231@gmail.com>
2024-05-16 22:47:33 +0530

    updated

--- price-tracker-/src/Pages/CoinPage.js
+++ price-tracker-/src/Pages/CoinPage.js
@@ -1,89 +1,3 @@
-// import React, { useEffect, useState } from 'react'
-// import { useParams } from 'react-router-dom'
-// import { CryptoState } from '../CryptoContext';
-// import { SingleCoin } from '../config/api';
-// import axios from 'axios';
-// import { Typography, makeStyles } from '@material-ui/core';
-// import CoinInfo from '../Components/CoinInfo';
-// // import ReactHtmlParser from "react-html-parser";
-
-// const useStyles = makeStyles((theme) => ({
-//     container:{
-//       display:"flex",
-//       [theme.breakpoints.down("md")]:{
-//         flexdirection:"column",
-//         alignItems:"center",
-//       },
-//     },
-//     sidebar:{
-//       width:"30%",
-//       [theme.breakpoints.down("md")]:{
-//         width:"100%",
-//       },
-//       display:"flex",
-//       flexdirection:"column",
-//       alignItems:"center",
-//       marginTop:25,
-//       borderRight:"2px solid grey"
-//     },
-//     heading:{
-//        fontWeight:"bold",
-//        marginBottom:20,
-//        fontFamily:"Montserrat",
-//     },
-// }));
-
-// const CoinPage = () => {
-//   const {id}  = useParams(); // for fetching the info of single coin
-  
-//   const[coin,setCoin] = useState();
-
-//   const {currency, symbol} = CryptoState();
-//   const classes = useStyles();
-
-//   const fetchCoin = async () => {
-//       const {data} = await axios.get(SingleCoin(id));
-
-//       setCoin(data);
-//   }
-
-//   console.log(coin);
-
-//   useEffect(() => {
-//     fetchCoin();
-//   },[])
-
-//   return (
-//     <div className={classes.container}>
-//        <div className={classes.sidebar}>
-//           <img
-//             src={coin?.image.large}
-//             alt={coin?.name}
-//             height="200"
-//             style = {{marginBottom:20}}
-//           />
-//           <Typography variant='h3' className={classes.heading}>
-//             {coin?.name}
-//           </Typography>
-//           {/* <Typography variant ="subtitle1" className={classes.description}>
-//             {ReactHtmlParser(coin?.description.en.split(". ")[0])}.
-//           </Typography> */}
-//           <Typography
-//             variant="subtitle1"
-//             className={classes.description}
-//             dangerouslySetInnerHTML={{ __html: coin?.description.en.split(". ")[0] }}
-//          />
-      
-//        </div>
-
-//        {/* chart */}
-//        <CoinInfo coin={coin}/>
-//     </div>
-//   )
-// }
-
-// export default CoinPage
-
 
 import { LinearProgress, makeStyles, Typography } from "@material-ui/core";
 import axios from "axios";
@@ -91,19 +5,19 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 // import ReactHtmlParser from "react-html-parser";
 import CoinInfo from "../Components/CoinInfo"; 
-import { SingleCoin } from "../config/api";
+import { SingleCoin } from "../config/api.js";
 import { numberWithCommas } from "../Components/CoinsTable";
 import { CryptoState } from "../CryptoContext";
 
 const CoinPage = () => {
-  const { id } = useParams();
+  const {id}= useParams();
   console.log("Id is ",id);
   const [coin, setCoin] = useState();
 
   const { currency, symbol } = CryptoState();
 
   const fetchCoin = async () => {
-    const data  = await axios.get(SingleCoin(id));
+    const data  = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}`);
     console.log("My data is ",data);
     setCoin(data.data);
   };
@@ -249,84 +163,6 @@ const CoinPage = () => {
     </div>
   );
 
-  // return (
-  //   <div >
-  //     <div >
-  //       <img
-  //         src={coin?.image.large}
-  //         alt={coin?.name}
-  //         height="200"
-  //         style={{ marginBottom: 20 }}
-  //       />
-  //       <Typography variant="h3" >
-  //         {coin?.name}
-  //       </Typography>
-  //       {/* <Typography variant="subtitle1" className={classes.description}>
-  //         {ReactHtmlParser(coin?.description.en.split(". ")[0])}.
-  //       </Typography> */}
-  //       <Typography
-  //          variant="subtitle1"
-          
-  //         dangerouslySetInnerHTML={{ __html: coin?.description.en.split(". ")[0] }}
-  //       />
-  //       <div >
-  //         <span style={{ display: "flex" }}>
-  //           <Typography variant="h5" >
-  //             Rank:
-  //           </Typography>
-  //           &nbsp; &nbsp;
-  //           <Typography
-  //             variant="h5"
-  //             style={{
-  //               fontFamily: "Montserrat",
-  //             }}
-  //           >
-  //             {numberWithCommas(coin?.market_cap_rank)}
-  //           </Typography>
-  //         </span>
-
-  //         <span style={{ display: "flex" }}>
-  //           <Typography variant="h5" >
-  //             Current Price:
-  //           </Typography>
-  //           &nbsp; &nbsp;
-  //           <Typography
-  //             variant="h5"
-  //             style={{
-  //               fontFamily: "Montserrat",
-  //             }}
-  //           >
-  //             {symbol}{" "}
-  //             {numberWithCommas(
-  //               coin?.market_data.current_price[currency.toLowerCase()]
-  //             )}
-  //           </Typography>
-  //         </span>
-  //         <span style={{ display: "flex" }}>
-  //           <Typography variant="h5" >
-  //             Market Cap:
-  //           </Typography>
-  //           &nbsp; &nbsp;
-  //           <Typography
-  //             variant="h5"
-  //             style={{
-  //               fontFamily: "Montserrat",
-  //             }}
-  //           >
-  //             {symbol}{" "}
-  //             {numberWithCommas(
-  //               coin?.market_data.market_cap[currency.toLowerCase()]
-  //                 .toString()
-  //                 .slice(0, -6)
-  //             )}
-  //             M
-  //           </Typography>
-  //         </span>
-  //       </div>
-  //     </div>
-  //     <CoinInfo coin={coin} />
-  //   </div>
-  // );
 };
 
 export default CoinPage;
